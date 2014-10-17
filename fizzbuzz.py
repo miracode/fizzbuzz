c59c29d5bd49af471319c09913724bb533ac6043
miracode <rascatim@gmail.com>
2014-10-17 10:15:30 -0700

    initialized project

---
+++ fizzbuzz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The module provides two function, fizzbuz and fizzbuz_extended
+"""
+
+def fizzbuzz(n):
+    """return a fizzbuzz-formatted representation of n"""
+    pass
