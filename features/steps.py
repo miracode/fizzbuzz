3e56776903de88407eda874a70462796eff41d94
miracode <rascatim@gmail.com>
2014-10-20 16:53:09 -0700

    first test passed

--- features/steps.py
+++ features/steps.py
@@ -3,14 +3,17 @@ from lettuce import world
 
 from fizzbuzz import fizzbuzz
 
+
 @step('the number (\d+)')
 def the_number(step, num):
     world.number = int(num)
 
-@step('I call FizzBuzz')
+
+@step('when I call fizzbuzz')
 def call_fizzbuzz(step):
     world.fb = fizzbuzz(world.number)
 
+
 @step('I see the output (\w+)')
 def compare(step, expected):
-    assert world.fb == expected, "Got %s" % expected
+    assert world.fb == expected, "Got %s" % world.fb
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,4 +5,13 @@
 
 def fizzbuzz(n):
     """return a fizzbuzz-formatted representation of n"""
-    pass
+    if n == 0:
+        return str(0)
+    out = ''
+    data = [(3, 'Fizz'), (5, 'Buzz')]
+    for divisor, replacement in data:
+        if not n % divisor:
+            out += replacement
+    if not out:
+        out = str(n)
+    return out
